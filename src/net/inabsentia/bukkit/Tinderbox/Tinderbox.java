a2d6508ef02c110f0849dbb671cd1210d24b0cba
crashvegas <crashvegas@gmail.com>
2011-05-11 09:47:23 -0400

    Initial commit

---
+++ src/net/inabsentia/bukkit/Tinderbox/Tinderbox.java
@@ -0,0 +1,19 @@
+package net.inabsentia.bukkit.Tinderbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tinderbox extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
